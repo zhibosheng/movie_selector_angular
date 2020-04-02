af56e2a21c0bb1a732fa88a84de22d5e92f7cdc4
zhibosheng <kage123zhibosheng@gmail.com>
2020-04-01 20:18:43 -0400

    fix signin problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { SigninComponent } from './signin/signin.component';
+import { SigninComponent } from './sign/signin.component';
 import { SignComponent } from './sign/sign.component';
 import { SignupComponent } from './sign/signup/signup.component';
 import { SignoutComponent } from './sign/signout/signout.component';
---
+++ src/app/footer/model/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    public userId: number;
+    public userName: string;
+    public password: string;
+    public email: string;
+    public phone: string;
+
+
+    constructor(){
+        
+    }
+}
