2c7edbb85ff889a16312f024b1cf3aa330111c60
zhibosheng <kage123zhibosheng@gmail.com>
2020-04-04 18:04:32 -0400

    init routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { SigninComponent } from './sign/signin.component';
+import { SigninComponent } from './sign/signin/signin.component';
 import { SignComponent } from './sign/sign.component';
 import { SignupComponent } from './sign/signup/signup.component';
 import { SignoutComponent } from './sign/signout/signout.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes:Routes = [
+  {path:'sign', component: SignComponent},
+  {path:'signin', component: SigninComponent},
+  {path:'signout', component: SignoutComponent},
+  {path:'signup', component: SignupComponent}   
+];
 
 @NgModule({
   declarations: [
@@ -22,7 +30,8 @@ import { SignoutComponent } from './sign/signout/signout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
